176f437fc98b80c5e94cf1d0f6b632d25849d3f4
akonenkamp <akonenkamp@gmail.com>
2017-05-25 19:11:45 -0700

    some Js added

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-function makeworkout(){
-  
-  
+var exercise = [
+  { 
+    "name": "burpee"
+   },
+  {
+    "name": "squat"
+  },
+  {
+    "name": "pushup"
+  },
+  {
+    "name": "run"
+  }
+];
+
+
+function validate {
+  var choice1  = document.getElementById("burpee").checked;
+  var choice2 = document.getElementById("squat").checked;
+  var choice3 = document.getElementById("pushup").checked;
+  var choice4 = document.getElementById("run").checked;
 }
