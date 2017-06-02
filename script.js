b47773c1b4682d9cc254d03726a9844b413eb7f2
akonenkamp <akonenkamp@gmail.com>
2017-06-01 18:28:46 -0700

    something that works

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
-var exercise = [
-  { 
-    "name": "burpee"
-   },
-  {
-    "name": "squat"
-  },
-  {
-    "name": "pushup"
-  },
-  {
-    "name": "run"
-  }
-];
-
+//var exercise = [
+//  { 
+//    "name": "burpee"
+//   },
+//  {
+//    "name": "squat"
+//  },
+//  {
+//    "name": "pushup"
+//  },
+//  {
+//    "name": "run"
+//  }
+//];
+//
+//
+//function changeText(){
+//var userInputname = document.getElementById('name').value;
+//var userInputcolor = Array.prototype.slice.call(document.querySelectorAll(".move:checked")).map(function(el) {
+//        return el.value;
+//    }).join(', ')
+//    
+//document.getElementById('output1').innerHTML = userInputname;
+//document.getElementById('output2').innerHTML = userInputcolor;
+//return false;
+//  
+//}
 
 function changeText(){
-var userInputname = document.getElementById('name').value;
+
 var userInputcolor = Array.prototype.slice.call(document.querySelectorAll(".move:checked")).map(function(el) {
         return el.value;
     }).join(', ')
     
-document.getElementById('output1').innerHTML = userInputname;
 document.getElementById('output2').innerHTML = userInputcolor;
 return false;
-  
 }
